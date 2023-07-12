facbd7b5b624d2b24e23f5e58c72decdce4467fd
theprinceofcodes <prince@underthehammer.com>
2023-07-12 18:29:12 +0100

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,17 @@
-import { socialLinks } from "../data"
+import { socialLinks, pageLinks } from "../data"
 const Footer = () => {
     return (
         <>
           <footer className="section footer">
                 <ul className="footer-links">
-                    <li>
-                    <a href="#home" className="footer-link">home</a>
-                    </li>
-                    <li>
-                    <a href="#about" className="footer-link">about</a>
-                    </li>
-                    <li>
-                    <a href="#services" className="footer-link">services</a>
-                    </li>
-                    <li>
-                    <a href="#featured" className="footer-link">featured</a>
-                    </li>
+                    {pageLinks.map((pagelink)=>{
+                        return (
+                            <li>
+                                <a href={pagelink.href} className="footer-link">{pagelink.text}</a>
+                            </li>
+                        )
+                    })}
+                   
                 </ul>
                 <ul className="footer-icons">
                     {socialLinks.map((socialLink)=>{
