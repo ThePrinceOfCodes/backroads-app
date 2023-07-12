a0171af6c3e9aa64d611a4ab89ad9aa8239d027c
theprinceofcodes <prince@underthehammer.com>
2023-07-12 15:01:42 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
 
 function App() {
   return (
     <>
-   
+      <Navbar />
+      <Hero />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   );
 }
